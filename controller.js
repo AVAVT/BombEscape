2cfb0040bbe884a891a1ecd02d396ac561499ecc
AVAVT <itsavavt@gmail.com>
2018-06-29 19:38:20 +0700

    Initial commit

---
+++ controller.js
@@ -0,0 +1,29 @@
+// const Directions = {
+//   UP: "UP",
+//   DOWN: "DOWN",
+//   LEFT: "LEFT",
+//   RIGHT: "RIGHT",
+//   STAY: "STAY"
+// }
+
+
+// const initialState = {
+//   boardWidth: 31,
+//   boardHeight: 31,
+//   playerPosition: {
+//     x: BOARD_WIDTH / 2,
+//     y: BOARD_HEIGHT / 2
+//   },
+//   bombs: [
+//     {
+//       timer: 2,
+//       x: 0,
+//       y: 0
+//     }
+//   ],
+//   gameOver: false
+// }
+
+const doTurn = (gameState) => {
+  return Directions.STAY;
+}
---
+++ system.js
@@ -0,0 +1,117 @@
+const BOARD_WIDTH = 31;
+const BOARD_HEIGHT = 31;
+const MIN_BOMBS_PER_TURN = 3;
+const DIFFICULTY_INCREASE_INTERVAL = 5;
+
+const Directions = {
+  UP: "UP",
+  DOWN: "DOWN",
+  LEFT: "LEFT",
+  RIGHT: "RIGHT",
+  STAY: "STAY"
+}
+
+const initialState = {
+  boardWidth: BOARD_WIDTH,
+  boardHeight: BOARD_HEIGHT,
+  playerPosition: {
+    x: BOARD_WIDTH / 2,
+    y: BOARD_HEIGHT / 2
+  },
+  bombs: [],
+  gameOver: false
+}
+
+const update = (gameState, turn, move = Directions.STAY) => {
+  const newPlayerPos = doMove(gameState.playerPosition, move);
+  const tickBombs = gameState.bombs
+    .filter(
+      b => b.timer > 0
+    )
+    .map(
+      b => Object.assign({}, b, { timer: b.timer - 1 })
+    );
+
+  const newBombs = [...tickBombs, ...createBombs(tickBombs, MIN_BOMBS_PER_TURN + turn / DIFFICULTY_INCREASE_INTERVAL, BOARD_WIDTH, BOARD_HEIGHT)];
+
+  const gameOver = newBombs.some(bomb => bomb.x === newPlayerPos.x && bomb.y === newPlayerPos.y) || newBombs.filter(bomb => bomb.timer === 0).some(bomb => Math.abs(bomb.x - newPlayerPos.x) + Math.abs(bomb.y - newPlayerPos.y) <= 2);
+
+  return {
+    playerPosition: newPlayerPos,
+    bombs: newBombs,
+    gameOver
+  };
+}
+
+const doMove = (playerPosition, move) => {
+  switch (move) {
+    case Directions.STAY:
+      return playerPosition;
+    case Directions.UP:
+      return playerPosition.y > 0
+        ? Object.assign({}, playerPosition, { y: playerPosition.y - 1 })
+        : playerPosition;
+    case Directions.DOWN:
+      return playerPosition.y < BOARD_HEIGHT - 1
+        ? Object.assign({}, playerPosition, { y: playerPosition.y + 1 })
+        : playerPosition;
+    case Directions.LEFT:
+      return playerPosition.x > 0
+        ? Object.assign({}, playerPosition, { x: playerPosition.x - 1 })
+        : playerPosition;
+    case Directions.RIGHT:
+      return playerPosition.x < BOARD_WIDTH - 1
+        ? Object.assign({}, playerPosition, { x: playerPosition.x + 1 })
+        : playerPosition;
+    default: return playerPosition;
+  }
+}
+
+const createBombs = (currentBombs, numberToCreate, boardWidth, boardHeight) => {
+  const possiblePosisions = Array.from({ length: boardWidth * boardHeight })
+    .map((item, index) => ({
+      timer: 2,
+      x: index % boardWidth,
+      y: index / boardWidth
+    }))
+    .filter(
+      position => currentBombs.every(bomb => bomb.x !== position.x && bomb.y !== position.y)
+    );
+  return knuthShuffle(possiblePosisions).slice(0, Math.min(numberToCreate, possiblePosisions.length));
+}
+
+function knuthShuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+const randomInt = (max, min) => Math.floor(Math.random() * (max - min) + min);
+
+const doGame = (gameState = initialState, turn = 0) => {
+  while (!gameState.gameOver && turn < 1000) {
+    gameState = update(gameState, turn, doTurn(gameState));
+    if (gameState.gameOver) {
+      alert("Gameover at turn " + turn);
+    }
+    turn++;
+  }
+}
+
+const startGame = () => doGame();
+
+window.onload = startGame;
